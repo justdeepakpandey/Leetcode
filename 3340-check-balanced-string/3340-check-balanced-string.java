17e2fe1457cfa07aaa3a2c0df50967f5e35d4bf6
Deepak Pandey <deepakpandey2797@gmail.com>
2026-06-28 18:41:01 +0530

    Time: 1 ms (98.46%), Space: 43.5 MB (64.2%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isBalanced(String num) {
+      char [] arr =num.toCharArray();
+      int ei=0;
+      int oi=0;
+      for(int i=0;i<arr.length;i++){
+        if(i%2==0){
+            ei=ei+arr[i]-'0';
+        }else{
+            oi=oi+arr[i]-'0';
+        }
+      }
+      if(ei!=oi){
+        return false;
+      }
+      return true;
+
+    }
+}
