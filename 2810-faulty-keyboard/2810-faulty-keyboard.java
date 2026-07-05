1d1b4a8142c849dee7c0bc1a3e3f37449c00453e
Deepak Pandey <deepakpandey2797@gmail.com>
2026-07-05 21:25:04 +0530

    Time: 3 ms (98.08%), Space: 46.8 MB (17.29%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='i'){
+                sb.append(s.charAt(i));
+
+            }else if(s.charAt(i)=='i'){
+                 sb.reverse();
+                
+               
+            }
+        }
+           
+        
+       return  sb.toString();
+    }
+}
