b61a6cdc130360539187367132e19814bbc7382d
Deepak Pandey <deepakpandey2797@gmail.com>
2026-07-16 13:43:57 +0530

    Time: 24 ms (8.93%), Space: 47.5 MB (65.28%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        int [] ans = new int[2];
         for(int i=0;i<nums.length;i++){
-            int c = nums[i];
-            map.put(c,map.getOrDefault(c,0)+1);
+        int c = nums[i];
+        map.put(c,map.getOrDefault(c,0)+1);
         }
-         for(int i:map.keySet()){
-            if(map.get(i)==2){
-                list.add(i);
-            }
-         }
-         HashSet<Integer> set = new HashSet<>();
-         for(int i:nums){
+        for(int i:map.keySet()){
+          if(map.get(i)==2){
+            ans[0]=i; 
+          }
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums){
             set.add(i);
-         }
-         for(int i=1;i<=nums.length;i++){
+        }
+        for(int i=1;i<=nums.length;i++){
             if(!set.contains(i)){
-                list.add(i);
+                ans[1]=i;
             }
-         }
-
-        int[] ans = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            ans[i]=list.get(i);
         }
         return ans;
     }
