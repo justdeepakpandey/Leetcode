92a433af1975f220c6f04e883d31b74cec71ef8b
Deepak Pandey <deepakpandey2797@gmail.com>
2026-07-18 21:39:07 +0530

    Time: 17 ms (81.51%), Space: 49.9 MB (17.27%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+       int i=0;
+       while(i<nums.length){
+        sum=sum+nums[i];
+        i=i+2;
+       }
+       return sum;
+    }
+}
