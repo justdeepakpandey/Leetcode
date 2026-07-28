f647d6fbe7f9955c66b1bb85a51cbec6db342bfb
Deepak Pandey <deepakpandey2797@gmail.com>
2026-07-28 11:34:34 +0530

    Time: 50 ms (16.71%), Space: 60.5 MB (7.76%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,57 +1,40 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Step 1: Sort the array
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate values of i
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        List<List<Integer>> ans = new ArrayList<>();
+       
+        for(int i=0;i<nums.length-2;i++){
+          
+            if(i>0&&nums[i]==nums[i-1]){
                 continue;
             }
-
-            int a = i + 1;
-            int b = nums.length - 1;
-
-            while (a < b) {
-
-                int sum = nums[i] + nums[a] + nums[b];
-
-                if (sum == 0) {
-
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[a]);
-                    temp.add(nums[b]);
-
-                    ans.add(temp);
-
+            int a=i+1;
+            int b=nums.length-1;
+            while(a<b){
+                 ArrayList<Integer> list = new ArrayList<>();
+                int sum=nums[i]+nums[a]+nums[b];
+                if(sum==0){
+                    list.add(nums[i]);
+                    list.add(nums[a]);
+                    list.add(nums[b]);
+                     ans.add(list);
                     a++;
                     b--;
-
-                    // Skip duplicate values of a
-                    while (a < b && nums[a] == nums[a - 1]) {
-                        a++;
-                    }
-
-                    // Skip duplicate values of b
-                    while (a < b && nums[b] == nums[b + 1]) {
-                        b--;
-                    }
-
-                } else if (sum < 0) {
+                
+                while(a<b&&nums[a]==nums[a-1]){
                     a++;
-                } else {
+                }
+                while(a<b && nums[b]==nums[b+1]){
                     b--;
                 }
+             }else if(sum>0){
+                b--;
+              }else{
+                a++;
+              }
+             
             }
         }
-
         return ans;
     }
 }
