2b79aae56644ec40020089c8ceb861b25c384fde
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-05 22:49:15 +0530

    Time: 6 ms (33.04%), Space: 44.9 MB (34.6%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,15 +1,14 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(!st.isEmpty()&&((st.peek()=='A'&& s.charAt(i)=='B')||(st.peek()=='C'&& s.charAt(i)=='D'))){
-                
-                    st.pop();
-                }else{
-                st.push(s.charAt(i));
-                }
+            char ch= s.charAt(i);
+            if(!st.isEmpty()&&st.peek()=='A'&&ch=='B'||!st.isEmpty()&&st.peek()=='C'&&ch=='D'){
+                st.pop();
+            }else{
+                st.push(ch);
             }
-            return st.size();
         }
-        
+        return st.size();
     }
+}
