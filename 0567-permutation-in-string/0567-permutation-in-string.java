2b487554138fd191d0cfd446bc7e585f9d44e3dc
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-07 21:31:54 +0530

    Time: 2674 ms (5.01%), Space: 47.2 MB (5.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-       
         if(s1.length()>s2.length()){
             return false;
         }
-     HashMap<Character,Integer> map1= new HashMap<>();
+        HashMap<Character,Integer> map= new HashMap<>();
         for(int i=0;i<s1.length();i++){
             char ch= s1.charAt(i);
-            map1.put(ch,map1.getOrDefault(ch,0)+1);
-
-
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
         for(int i=0;i<s2.length();i++){
-             HashMap<Character,Integer> map2= new HashMap<>();
+            HashMap<Character,Integer> map2= new HashMap<>();
             for(int j=i;j<s2.length();j++){
-            char c=s2.charAt(j);
-             map2.put(c,map2.getOrDefault(c,0)+1);
-             if(j-i+1==s1.length()){
-                if(map1.equals(map2)){
+                char ch= s2.charAt(j);
+                map2.put(ch,map2.getOrDefault(ch,0)+1);
+                if(j-i+1==s1.length()){
+                if(map.equals(map2)){
                     return true;
                 }
-             }
-             if(map2.size()>map1.size()){
-                break;
-             }
-
+                }
+                if(map2.size()>map.size()){
+                    break;
+                }
             }
         }
         return false;
