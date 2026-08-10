ca827457abd56cc38458bfb95ebf6ddb78888118
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-10 09:41:17 +0530

    Time: 1 ms (100%), Space: 46.6 MB (38.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,46 +10,23 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ArrayList<Integer> list1= new ArrayList<>();
-        ArrayList<Integer> list2 =new ArrayList<>();
-        while(l1!=null){
-            list1.add(l1.val);
-            l1=l1.next;
-        }
-        while(l2!=null){
-            list2.add(l2.val);
-            l2=l2.next;
-        }
-        int i=0;
-        int j=0;
+        ListNode dummy=new ListNode(0);
+        ListNode curr =dummy;
         int carry=0;
-        ArrayList<Integer> list3=new ArrayList<>();
-        while(i<list1.size()||j<list2.size()){
-            int a=0;
-            int b=0;
-            if(i<list1.size()){
-                a=list1.get(i);
-            }
-            if(j<list2.size()){
-                b=list2.get(j);
-            }
-            int sum=a+b+carry;
-            list3.add(sum%10);
-            carry=sum/10;
-           
-            i++;
-            j++;
+        while(l1!=null||l2!=null||carry!=0){
+          int sum=carry;
+          if(l1!=null){
+            sum+=l1.val;
+            l1=l1.next;
+          }
+           if(l2!=null){
+            sum+=l2.val;
+            l2=l2.next;
+          }
+          carry=sum/10;
+          curr.next=new ListNode(sum%10);
+          curr=curr.next;
         }
-         if(carry>0){
-                list3.add(carry);
-            }
-
-            ListNode dummy=new ListNode(0);
-            ListNode curr=dummy;
-            for(int digit:list3){
-                curr.next=new ListNode(digit);
-                curr=curr.next;
-            }
-            return dummy.next;
+        return dummy.next;
     }
 }
