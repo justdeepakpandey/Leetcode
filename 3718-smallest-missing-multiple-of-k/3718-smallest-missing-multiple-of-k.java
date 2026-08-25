da21d6783b6827435f441ae12fa5c4400b4ff1c0
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-25 20:33:24 +0530

    Time: 1 ms (82%), Space: 44.8 MB (83.31%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -6,13 +6,13 @@ class Solution {
             list.add(i);
         }
         for(int i=1;i<Integer.MAX_VALUE;i++){
-        while(!list.contains(k*i)){
+     
             if(!list.contains(k*i)){
                 return k*i;
                 
             }
 
-        }
+        
         }
         return -1;
     }
