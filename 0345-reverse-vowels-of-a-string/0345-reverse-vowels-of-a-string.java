eca6540b5184454317624653aeb29a7851c20feb
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-31 10:10:19 +0530

    Time: 3 ms (65.72%), Space: 45.5 MB (98.89%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,14 +1,18 @@
 class Solution {
+     public boolean isVowel(char c){
+            return c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u' ||c=='A' ||c=='E' ||c=='I' ||c=='O' ||c=='U';
+        }
     public String reverseVowels(String s) {
+       
        StringBuilder sb = new StringBuilder(s);
        int i=0;
        int j=sb.length()-1;
        while(i<j){
-        while(i<j&&sb.charAt(i)!='a'&&sb.charAt(i)!='e'&&sb.charAt(i)!='i'&&sb.charAt(i)!='o'&&sb.charAt(i)!='u'&&sb.charAt(i)!='A'&&sb.charAt(i)!='E'&&sb.charAt(i)!='I'&&sb.charAt(i)!='O'&&sb.charAt(i)!='U'){
+        while(i<j&&!isVowel(sb.charAt(i))){
             i++;
             
         }
-         while(i<j&&sb.charAt(j)!='a'&&sb.charAt(j)!='e'&&sb.charAt(j)!='i'&&sb.charAt(j)!='o'&&sb.charAt(j)!='u'&&sb.charAt(j)!='A'&&sb.charAt(j)!='E'&&sb.charAt(j)!='I'&&sb.charAt(j)!='O'&&sb.charAt(j)!='U'){
+         while(i<j&&!isVowel(sb.charAt(j))){
             j--;
         }
         if(i<j){
