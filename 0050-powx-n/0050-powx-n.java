057b3767b512f8f151f3fe02e5127330cf51480b
Deepak Pandey <deepakpandey2797@gmail.com>
2026-08-31 21:51:10 +0530

    Time: 0 ms (100%), Space: 47.7 MB (78.45%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long expo=n;
+        if(expo<0){
+            x=1/x;
+            expo=-expo;
+
+        }
+        double ans =1;
+        while(expo>0){
+              if(expo%2!=0){
+                ans*=x;
+              }
+              x*=x;
+              expo/=2;
+        }
+        return ans;
+    }
+}
