21341a1316210ba07172572de275945aaa5bba7f
Deepak Pandey <deepakpandey2797@gmail.com>
2026-09-07 18:07:44 +0530

    Time: 2 ms (97.77%), Space: 71.9 MB (98.77%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,26 +1,40 @@
 class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        int five=0;
-        int ten=0;
-        for(int i=0;i<bills.length;i++){
-            if(bills[i]==5){
-                five++;
-            }else if(bills[i]==10){
-                if(five>0){
-                    five--;
-                }else{
+    public boolean lemonadeChange(int[] nums) {
+        if(nums[0]!=5){
+            return false;
+
+        }
+        int cf=0;
+        int ct=0;
+        int cty=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==5){
+                cf++;
+            }
+            else if(nums[i]==10){
+                if(cf>0){
+                    cf--;
+                }
+                else{
                     return false;
                 }
-                ten++;
-            }else if(five>0&&ten>0){
-                five--;
-                ten--;
-            }else if(five>=3){
-                five=five-3;
-            }else{
-                return false;
+                ct++;
             }
-        }
-        return true;
+            else{
+                if(cf>0&&ct>0){
+                    cf--;
+                    ct--;
+                }
+                else if(cf>=3){
+                    cf=cf-3;
+                }
+                else{
+                    return false;
+                }
+            }
+                
+        }      
+               
+          return true;
     }
 }
