54b886fe815b4ed85d6c869ca242c04374a61321
Deepak Pandey <deepakpandey2797@gmail.com>
2026-09-15 18:49:25 +0530

    Time: 79 ms (5.65%), Space: 47 MB (7.98%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+           ArrayList<Integer> list= new ArrayList<>();
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int i=0;i<digits.length;i++){
+            int a=digits[i];
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+    for(int i=100;i<999;i++){
+        if(i%2==0){
+            int temp=i;
+            HashMap<Integer,Integer> map2= new HashMap<>();
+            while(temp>0){
+                int digit=temp%10;
+                map2.put(digit,map2.getOrDefault(digit,0)+1);
+                temp=temp/10;
+            }
+            boolean possible= true;
+        for(int test:map2.keySet()){
+            if(!map.containsKey(test)||map2.get(test)>map.get(test)){
+                possible=false;
+               break;
+
+            }
+        }
+        if(possible){
+            list.add(i);
+        }
+         
+        }else{
+            continue;
+        }
+    }
+   
+    int [] arr=new int[list.size()];
+    for(int i=0;i<list.size();i++){
+        arr[i]=list.get(i);
+    }
+    return arr.length;
+    }
+}
